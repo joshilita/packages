f5f881b5a84ab18bb7dcafc24ec9f31d0c0d2e1a
joshilita <52619112+joshilita@users.noreply.github.com>
2022-11-07 22:49:11 -0500

    Update run.sh

--- parrot/run.sh
+++ parrot/run.sh
@@ -1,3 +1,9 @@
+ trap printout SIGINT
+    printout() {
+	echo ""
+        echo "Done, I hope you enjoyed it!"
+        exit
+    }
 echo "Press CTRL-C anytime to stop. (Starts in 3 seconds)"
 sleep 3
 curl parrot.live
