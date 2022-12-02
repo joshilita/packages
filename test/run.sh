46a853dcc836ccc63125fdb35246ee071570ef59
joshilita <52619112+joshilita@users.noreply.github.com>
2022-12-02 11:18:48 -0500

    Update run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,8 @@
 echo "TEST COMPLETE"
-
+variable1=$1
+if [ "$variable1" ]; then
+echo "hello $variable1"
+fi
 if [ -f ~/NewOSv3/Packages/test/startup ]; then
 echo "package installed yay"
 rm -rf ~/NewOSv3/Packages/test/startup
